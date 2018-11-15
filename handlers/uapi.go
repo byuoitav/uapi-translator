5c46213825324a0882058df8bb52211865f2b4b6
jpw547 <jwerner547@gmail.com>
2018-11-14 17:49:40 -0700

    some auth stuff hopefully

--- handlers/uapi.go
+++ handlers/uapi.go
@@ -78,16 +78,16 @@ func GetState(context echo.Context) error {
 	roomID := context.Param("roomID")
 
 	// check the JWT token
-	// ok, err := helpers.AuthenticatedByJWT(context, "read-state")
-	// if err != nil {
-	// 	log.L.Error(color.HiRedString("Brwaap! Error authenticating! : %s", err.Error()))
-	// 	return context.JSON(http.StatusInternalServerError, "There was a problem")
-	// }
+	ok, err := helpers.AuthenticatedByJWT(context, "read-state")
+	if err != nil {
+		log.L.Error(color.HiRedString("Brwaap! Error authenticating! : %s", err.Error()))
+		return context.JSON(http.StatusInternalServerError, "There was a problem")
+	}
 
-	// if !ok {
-	// 	log.L.Error(color.HiRedString("Brwaap! The scalawag did not meet authentication checks!"))
-	// 	return context.JSON(http.StatusForbidden, "Unauthorized")
-	// }
+	if !ok {
+		log.L.Error(color.HiRedString("Brwaap! The scalawag did not meet authentication checks!"))
+		return context.JSON(http.StatusForbidden, "Unauthorized")
+	}
 
 	log.L.Debugf("Brwaap! Getting state for %s!", roomID)
 
--- helpers/auth.go
+++ helpers/auth.go
@@ -1,111 +1,62 @@
 package helpers
 
 import (
-	"encoding/json"
-	"errors"
-	"fmt"
-	"io/ioutil"
-	"net/http"
+	"os"
 
+	"github.com/byuoitav/common/jsonhttp"
 	"github.com/byuoitav/common/log"
-	"github.com/byuoitav/common/v2/auth"
-	jwt "github.com/dgrijalva/jwt-go"
+	"github.com/byuoitav/endpoint-authorization-controller/base"
 	"github.com/labstack/echo"
 )
 
 // AuthenticatedByJWT scans the JWT token and processes to see if the user is granted access to the resource.
 func AuthenticatedByJWT(context echo.Context, role string) (bool, error) {
 	roomID := context.Param("room")
+
+	log.L.Debug("Arg matey, what's yer JWT like?")
 	token := context.Request().Header.Get("X-jwt-assertion")
 
 	if len(token) > 0 { // Proceed if we found a token
-		valid, err := validate(token) // Validate the existing token
+		// build a base.Request object with the information
+		jwtRequest := base.Request{
+			AccessKey: "",
+			UserInformation: base.UserInformation{
+				ResourceID: roomID,
+				CommonInfo: base.CommonInfo{
+					ID:           "",
+					AuthMethod:   "wso2",
+					ResourceType: "room",
+					Data:         []byte(token),
+				},
+			},
+		}
+
+		log.L.Debug("Time to see if yer full o' barnacles or not!")
+		// create an HTTP request
+		req, err := jsonhttp.CreateRequest("POST", os.Getenv("ENDPOINT_AUTHORIZATION_URL"), jwtRequest, nil)
 		if err != nil {
-			log.L.Debug("Invalid WSO2 information")
+			log.L.Errorf("failed to make request - %s", err.Error())
 			return false, err
 		}
 
-		if valid {
-			log.L.Debug("WSO2 validated successfully")
+		// send it to the EAC
+		var resp base.Response
+		jsonhttp.ExecuteRequest(req, &resp, 60)
 
-			// check user's roles in our database
-			// TODO: get the real user and access key and stuff...
-			ok, err := auth.CheckRolesForUser("user", "accessKey", role, roomID, "room")
-			if err != nil {
-				return false, err
+		// read the response to see if the token is valid
+		if resp.Permissions == nil {
+			log.L.Errorf("no permissions found, to Davy Jones' locker with ye!")
+			return false, nil
+		}
+
+		log.L.Debug("Searching the map for treasure....")
+		for _, r := range resp.Permissions[roomID] {
+			if r == role {
+				return true, nil
 			}
-
-			if !ok {
-				return false, nil
-			}
-
-			return true, nil
 		}
 	}
 
+	log.L.Debug("Stowaway! The Cap'n'll be hear'n about this!")
 	return false, nil
-}
-
-type keys struct {
-	Keys []struct {
-		E   string   `json:"e"`
-		Kty string   `json:"kty"`
-		Use string   `json:"use"`
-		Kid string   `json:"kid"`
-		N   string   `json:"n"`
-		X5C []string `json:"x5c"`
-	} `json:"keys"`
-}
-
-func validate(token string) (bool, error) {
-	parsedToken, err := jwt.Parse(token, func(parsedToken *jwt.Token) (interface{}, error) {
-		if parsedToken.Method.Alg() != "RS256" { // Check that our keys are signed with RS256 as expected (https://auth0.com/blog/2015/03/31/critical-vulnerabilities-in-json-web-token-libraries/)
-			return nil, fmt.Errorf("Unexpected signing method: %v", parsedToken.Header["alg"]) // This error never gets returned to the user but may be useful for debugging/logging at some point
-		}
-
-		// Look up key
-		key, err := lookupSigningKey()
-		if err != nil {
-			return nil, err
-		}
-
-		// Unpack key from PEM encoded PKCS8
-		return jwt.ParseRSAPublicKeyFromPEM(key)
-	})
-
-	log.L.Debugf("%v", parsedToken)
-
-	if parsedToken.Valid {
-		return true, nil
-	} else if validationError, ok := err.(*jwt.ValidationError); ok {
-		if validationError.Errors&jwt.ValidationErrorMalformed != 0 {
-			return false, errors.New("Authorization token is malformed")
-		} else if validationError.Errors&(jwt.ValidationErrorExpired|jwt.ValidationErrorNotValidYet) != 0 {
-			return false, errors.New("Authorization token is expired")
-		}
-	}
-
-	return false, errors.New("WSO2 JWT token not authorized")
-}
-
-func lookupSigningKey() ([]byte, error) {
-	response, err := http.Get("https://api.byu.edu/.well-known/byucerts")
-	if err != nil {
-		return nil, err
-	}
-
-	allKeys := keys{}
-	responseBody, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		return nil, err
-	}
-
-	err = json.Unmarshal(responseBody, &allKeys)
-	if err != nil {
-		return nil, err
-	}
-
-	certificate := "-----BEGIN CERTIFICATE-----\n" + allKeys.Keys[0].X5C[0] + "\n-----END CERTIFICATE-----"
-	log.L.Debug(certificate)
-	return []byte(certificate), nil
 }
